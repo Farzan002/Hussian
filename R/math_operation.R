ddc9ac80341d16d89b6639d24bc9d25c7d08bcb8
Farzan Hussain <113101934+Farzan002@users.noreply.github.com>
2022-11-25 22:34:43 +0530

    Add files via upload

---
+++ R/math_operation.R
@@ -0,0 +1,31 @@
+#' Provides mode for a distribution vector
+#' @param add1 Addition.
+#' @param sub1 Subtraction
+#' @param mul1 Multiplication
+#' @param div1 division
+#' @return c Respective outputs.
+
+
+
+add1 <- function(a,b){
+  c = a+b
+  return(c)
+}
+
+
+sub1 <- function(a,b){
+  c = a-b
+  return(c)
+}
+
+
+mul1 <- function(a,b){
+  c = a*b
+  return(c)
+}
+
+
+div1 <- function(a,b){
+  c = a/b
+  return(c)
+}
